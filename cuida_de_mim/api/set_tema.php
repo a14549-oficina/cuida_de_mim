e4925050e0ea6df470b7893fc9e488929ebab6b7
a14549-oficina <a14549@oficina.pt>
2026-06-14 23:29:21 +0100

    Delete cuida_de_mim directory

--- cuida_de_mim/api/set_tema.php
+++
@@ -1,4 +0,0 @@
-<?php
-endpoint para evitar erros 404
-header('Content-Type: application/json');
-echo json_encode(['ok' => true]);
--- cuida_de_mim/cuidador/sair.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/config.php';
-unset($_SESSION['cuidador_utente_id']);
-header('Location: ' . BASE . 'index.php'); exit;
